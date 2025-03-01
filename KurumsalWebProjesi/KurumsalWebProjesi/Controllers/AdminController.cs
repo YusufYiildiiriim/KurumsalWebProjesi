917043f4c312e3d54387bc77e5e85e230b62823c
Yusuf Yıldırım <yusuf.yiildiiriim@gmail.com>
2025-03-01 16:06:03 +0300

    veritabanıbağlantısıyapılacak

---
+++ KurumsalWebProjesi/KurumsalWebProjesi/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿
+using KurumsalWebProjesi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KurumsalWebProjesi.Controllers
+{
+    public class AdminController : Controller
+    {
+        KurumsalDB db = new KurumsalDB();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var sorgu = db.Kategoris.ToList();
+
+            return View(sorgu);
+        }
+    }
+}
