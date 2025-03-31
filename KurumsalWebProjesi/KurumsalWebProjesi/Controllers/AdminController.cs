95624ffee4214194ab2b886a90034d52a91a00d4
Yusuf Yıldırım <yusuf.yiildiiriim@gmail.com>
2025-03-31 13:01:48 +0300

    back-end oluşturuldu

--- KurumsalWebProjesi/KurumsalWebProjesi/Controllers/AdminController.cs
+++ KurumsalWebProjesi/KurumsalWebProjesi/Controllers/AdminController.cs
@@ -1,11 +1,13 @@
 ﻿
 using KurumsalWebProjesi.Models;
 using KurumsalWebProjesi.Models.DataContext;
+using KurumsalWebProjesi.Models.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 
 namespace KurumsalWebProjesi.Controllers
 {
@@ -18,5 +20,35 @@ namespace KurumsalWebProjesi.Controllers
             var sorgu = db.Kategori.ToList();
             return View(sorgu);
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+            var login = db.Admin.Where(x => x.Eposta == admin.Eposta).SingleOrDefault();
+            if(login.Eposta==admin.Eposta && login.Sifre == admin.Sifre)
+            {
+                Session["adminid"] = login.AdminId;
+                Session["eposta"] = login.Eposta;
+                return RedirectToAction("Index", "Admin");
+            }
+            ViewBag.Uyari = "Kullanıcı adı ya da şifre yanlış"; 
+            return View(admin);
+
+        }
+        public ActionResult LogOut()
+        {
+            Session["adminid"] = null;
+            Session["eposta"] = null;
+            Session.Abandon();
+            return RedirectToAction("Login", "Admin");
+        }
+
+
     }
+
+
 }
