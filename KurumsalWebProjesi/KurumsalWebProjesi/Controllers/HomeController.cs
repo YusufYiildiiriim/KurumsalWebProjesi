bfc5d31ba69a50222c2692e45cb86a99f9f02c15
Yusuf Yıldırım <yusuf.yiildiiriim@gmail.com>
2025-04-04 15:24:40 +0300

    Blog sayfası oluşturuldu

--- KurumsalWebProjesi/KurumsalWebProjesi/Controllers/HomeController.cs
+++ KurumsalWebProjesi/KurumsalWebProjesi/Controllers/HomeController.cs
@@ -3,7 +3,9 @@ using KurumsalWebProjesi.Models.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 
 namespace KurumsalWebProjesi.Controllers
@@ -14,8 +16,9 @@ namespace KurumsalWebProjesi.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.Hizmetler = db.Hizmet.ToList().OrderByDescending(x => x.HizmetId);
 
-            ViewBag.Hizmetler = db.Hizmet.ToList().OrderByDescending(x=>x.HizmetId);
+
             return View();
         }
         public ActionResult SliderPartial()
@@ -25,6 +28,54 @@ namespace KurumsalWebProjesi.Controllers
         public ActionResult HizmetPartial()
         {
             return View(db.Hizmet.ToList());
+        }
+        public ActionResult Hakkimizda()
+        {
+
+            return View(db.Hakkimizda.SingleOrDefault());
+        }
+        public ActionResult Hizmetlerimiz()
+        {
+            return View(db.Hizmet.ToList().OrderByDescending(x => x.HizmetId));
+        }
+        public ActionResult Iletisim()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Iletisim(string adsoyad = null, string email = null, string konu = null, string mesaj = null)
+        {
+            if (adsoyad != null && email != null)
+            {
+                WebMail.SmtpServer = "smtp.gmail.com";
+                WebMail.EnableSsl = true;
+                WebMail.UserName = "cdogrulama@gmail.com";
+                WebMail.Password = "yusuf719";
+                WebMail.SmtpPort = 587;
+                WebMail.Send("cdogrulama@gmail.com", konu, email+"</br>"+mesaj );
+                ViewBag.Uyari = "Mesajınız basaşrıyla gönderilmiştir";
+
+
+            }
+            else
+            {
+                ViewBag.Uyari = "Hata oluştu tekrar deneyiniz";
+
+            }
+
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View(db.Blog.Include("Kategori").ToList().OrderByDescending(x=>x.BlogId));
+        }
+
+        public ActionResult FooterPartial()
+        {
+            ViewBag.Iletisim = db.Iletisim.SingleOrDefault();
+            ViewBag.Hizmetler = db.Hizmet.ToList().OrderByDescending(x => x.HizmetId);
+            ViewBag.Blog = db.Blog.ToList().OrderByDescending(x => x.BlogId);
+            return PartialView();
         }
 
 
